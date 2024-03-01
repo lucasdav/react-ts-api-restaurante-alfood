eb961748c870a0168785651ce886ace89dcbbf26
lucasdav <lucassouzadavanso@gmail.com>
2024-03-01 19:37:50 -0300

    preparando a administracao

--- alfood-main/src/componentes/ListaRestaurantes/Restaurante/index.tsx
+++ alfood-main/src/componentes/ListaRestaurantes/Restaurante/index.tsx
@@ -1,12 +1,23 @@
+import { useEffect, useState } from 'react';
 import IRestaurante from '../../../interfaces/IRestaurante';
 import Prato from '../Prato';
 import estilos from './Restaurante.module.scss';
+import axios from 'axios';
+import IPrato from '../../../interfaces/IPrato';
 
 interface RestauranteProps {
   restaurante: IRestaurante
 }
 
 const Restaurante = ({ restaurante }: RestauranteProps) => {
+
+  const [pratos, setPratos] = useState<IPrato[]>()
+  useEffect(() => {
+    axios.get<IPrato[]>(`http://localhost:8000/api/v1/restaurantes/${restaurante.id}/pratos/`)
+      .then(resposta => {
+        setPratos(resposta.data)
+      })
+  }, [restaurante.id])
 
   return (<section className={estilos.Restaurante}>
     <div className={estilos.Titulo}>
